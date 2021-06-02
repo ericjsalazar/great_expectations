2def55415a6b2fa44c69a5f4a9ffbb3fc7b251b3
Shinnnyshinshin <will@superconductive.com>
2021-06-02 16:26:27 -0700

    first push

--- docs/guides/connecting_to_your_data/cloud/s3/spark.md
+++ docs/guides/connecting_to_your_data/cloud/s3/spark.md
@@ -2,4 +2,3 @@
 title: How to connect to your data on a S3 using spark
 ---
 
-**TODO** This is a stub
---
+++ tests/integration/docusaurus/connecting_to_your_data/cloud/spark_s3_python_example.py
@@ -0,0 +1,10 @@
+from ruamel import yaml
+
+import great_expectations as ge
+from great_expectations.core.batch import BatchRequest, RuntimeBatchRequest
+
+from ruamel import yaml
+
+import great_expectations as ge
+
+context = ge.get_context()
