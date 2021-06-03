296e63bbea1f72e21a0947fbfc720753dae5530e
Shinnnyshinshin <will@superconductive.com>
2021-06-02 17:08:57 -0700

    RuntimeBatchRequest example

--- tests/integration/docusaurus/connecting_to_your_data/cloud/spark_s3_yaml_example.py
+++ tests/integration/docusaurus/connecting_to_your_data/cloud/spark_s3_yaml_example.py
@@ -34,38 +34,35 @@ data_connectors:
 datasource_yaml = datasource_yaml.replace("<PATH_TO_YOUR_S3_BUCKET_HERE>", "superconductive-public")
 datasource_yaml = datasource_yaml.replace("<PATH_TO_YOUR_S3_FOLDER_HERE>", "data/taxi_yellow_trip_data_samples/")
 
-print("datasource_yaml")
-print(datasource_yaml)
-print("~~~~~~~~")
 context.test_yaml_config(datasource_yaml)
-print("~~~~~~~~")
+
 context.add_datasource(**yaml.load(datasource_yaml))
 
-# # Here is a RuntimeBatchRequest using a path to a single CSV file
-# batch_request = RuntimeBatchRequest(
-#     datasource_name="taxi_datasource",
-#     data_connector_name="default_runtime_data_connector_name",
-#     data_asset_name="<YOUR_MEANGINFUL_NAME>",  # this can be anything that identifies this data_asset for you
-#     runtime_parameters={"path": "<PATH_TO_YOUR_DATA_HERE>"},  # Add your path here.
-#     batch_identifiers={"default_identifier_name": "something_something"},
-# )
-#
-# # Please note this override is only to provide good UX for docs and tests.
-# # In normal usage you'd set your path directly in the BatchRequest above.
-# batch_request.runtime_parameters["path"] = "./data/yellow_trip_data_sample_2019-01.csv"
-#
-# context.create_expectation_suite(
-#     expectation_suite_name="test_suite", overwrite_existing=True
-# )
-# validator = context.get_validator(
-#     batch_request=batch_request, expectation_suite_name="test_suite"
-# )
-# print(validator.head())
-#
-# NOTE: The following code is only for testing and can be ignored by users.
-#assert isinstance(validator, ge.validator.validator.Validator)
+# Here is a RuntimeBatchRequest using a path to a single CSV file
+batch_request = RuntimeBatchRequest(
+    datasource_name="my_s3_datasource",
+    data_connector_name="default_runtime_data_connector_name",
+    data_asset_name="TestingMe",  # this can be anything that identifies this data_asset for you
+    runtime_parameters={"path": "<PATH_TO_YOUR_DATA_HERE>"},  # Add your path here.
+    batch_identifiers={"default_identifier_name": "something_something"},
+)
 
-# Here is a BatchRequest naming a data_asset
+# Please note this override is only to provide good UX for docs and tests.
+# In normal usage you'd set your path directly in the BatchRequest above.
+batch_request.runtime_parameters["path"] = "s3://superconductive-public/data/taxi_yellow_trip_data_samples/yellow_trip_data_sample_2019-01.csv"
+
+context.create_expectation_suite(
+    expectation_suite_name="test_suite", overwrite_existing=True
+)
+validator = context.get_validator(
+    batch_request=batch_request, expectation_suite_name="test_suite"
+)
+print(validator.head())
+
+#NOTE: The following code is only for testing and can be ignored by users.
+assert isinstance(validator, ge.validator.validator.Validator)
+
+# # Here is a BatchRequest naming a data_asset
 batch_request = BatchRequest(
     datasource_name="my_s3_datasource",
     data_connector_name="default_inferred_data_connector_name",
